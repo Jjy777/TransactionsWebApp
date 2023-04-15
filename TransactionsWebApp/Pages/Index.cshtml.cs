519ea0d6b198c79e01942c39bb748c1ad9262732
Jjy777 <logus14june@gmail.com>
2023-04-16 01:04:01 +0800

    Add bad request and validation methods for uploaded file size and file format. Add file upload inputs in page.

--- TransactionsWebApp/Pages/Index.cshtml.cs
+++ TransactionsWebApp/Pages/Index.cshtml.cs
@@ -9,6 +9,8 @@ namespace TransactionsWebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _db;
+        private readonly int _maxFileSize = 220000 ;
+            /*1 * 1024 * 1024;*/
 
         public IEnumerable<Transactions> Transactions { get; set; }
         public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext db)
@@ -22,9 +24,32 @@ namespace TransactionsWebApp.Pages
         {
             Transactions = _db.Transactions;
         }
-        public IActionResult OnPost() 
+        public IActionResult OnPost(IFormFile file) 
         {
+            if (file != null)
+            {
+                string strFileName = Path.GetFileName(file.FileName);
+                string strExtension = Path.GetExtension(strFileName);
+                if (file.Length > _maxFileSize)
+                {
+                    return BadRequest("Maximum allowed file size is 1 MB.");
+                }
+                else {
+                    if (strExtension == ".xml" || strExtension == ".csv")
+                    {
+                        return new OkResult();
+
+                    }
+                    else
+                    {
+                        return BadRequest("Only CSV file and XML file can be accepted.");
+                       
+                    }
+
+                }
+            }
             return Page();
         }
+        
     }
 }
